b50875038cd2c361d623938d4191cc2618e952a9
royaghavami <royasara99@gmail.com>
2022-04-13 03:02:32 -0700

    receiving data using model from database and showing them in view

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from .models import Article
 
 
 def home(request):
     context = {
-        "articles": [
-            {
-                "title": "لیگ قهرمانان اروپا",
-                "description": "چرت و پرت می نویسم همانا رستگار شوم:)",
-                "image": "https://hamsan-cdn-a.yektanet.com/media/CACHE/images/items/image_ebb59fb4-cef8-4d6f-b697-188949b392e2__1daEtXrrjk/90/375x250.jpeg"
-            },
-            {
-                "title": "لیگ قهرمانان آسیا",
-                "description": "What does the fox say?",
-                "image": "https://www.thespruce.com/thmb/JObhDcNNBALGVyF9S7bFoQwoXsI=/941x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/plants-with-big-flowers-4138211-hero-b10becb169064cc4b3c7967adc1b22e1.jpg"
-            }
-        ]
-
+        "articles": Article.objects.filter(status='p').order_by('-publish')
+        #if we wanted to show only last three articles:
+        #"articles": Article.objects.filter(status='p').order_by('-publish')[:3]
     }
     return render(request, "blog/home.html", context)
 
